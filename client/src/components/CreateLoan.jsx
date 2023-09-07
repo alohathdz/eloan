7ccd2c4b09e4e514ab85e8680636a87265921e6c
alohathdz <57053156+alohathdz@users.noreply.github.com>
2023-09-07 15:57:48 +0700

    เพิ่ม validate create loan

--- client/src/components/CreateLoan.jsx
+++ client/src/components/CreateLoan.jsx
@@ -7,7 +7,17 @@ import { useNavigate, useParams } from 'react-router-dom'
 function CreateLoan() {
 
     const { id } = useParams();
+
     const navigate = useNavigate();
+
+    const [name, setName] = useState('');
+    const getName = async () => {
+        await axios.get('http://localhost:8081/member/edit/' + id)
+            .then(res => {
+                setName(res.data[0].name);
+            }).catch(err => console.log(err))
+    }
+
     const [values, setValues] = useState({
         amount: '',
         rate: '',
@@ -15,25 +25,33 @@ function CreateLoan() {
         id: id
     });
 
+    const [errors, setErrors] = useState({})
+
+    const handleChange = async (e) => {
+        const { name, value } = e.target
+        setValues({ ...values, [name]: value })
+    }
+
     const handleCreate = async (e) => {
         e.preventDefault();
 
-        await axios.post('http://localhost:8081/loan/create', values)
-            .then(res => {
-                Swal.fire({
-                    icon: "success",
-                    text: "Create Loan Successfully."
-                });
-                navigate(-1);
-            }).catch(err => console.log(err))
-    }
+        const validationError = {}
+        if (!values.amount.trim()) { validationError.amount = "กรุณาใส่ยอดที่กู้" }
+        if (!values.rate.trim()) { validationError.rate = "กรุณาใส่อัตราดอกเบี้ย" }
+        if (!values.start_date.trim()) { validationError.start_date = "กรุณาเลือกวันที่กู้" }
 
-    const [name, setName] = useState('');
-    const getName = async () => {
-        await axios.get('http://localhost:8081/member/edit/' + id)
-        .then(res => {
-            setName(res.data[0].name);
-        }).catch(err => console.log(err))
+        setErrors(validationError)
+
+        if (Object.keys(validationError).length === 0) {
+            await axios.post('http://localhost:8081/loan/create', values)
+                .then(res => {
+                    Swal.fire({
+                        icon: "success",
+                        text: "Create Loan Successfully."
+                    });
+                    navigate(-1);
+                }).catch(err => console.log(err))
+        }
     }
 
     useEffect(() => {
@@ -54,15 +72,18 @@ function CreateLoan() {
                                         <Col>
                                             <Form.Group controlId="Amount" className='mb-3'>
                                                 <Form.Label>ยอดกู้</Form.Label>
-                                                <Form.Control type="number" onChange={e => setValues({ ...values, amount: e.target.value })} required/>
+                                                <Form.Control type="number" name="amount" onChange={handleChange} />
+                                                {errors.amount && <p style={{color: 'red'}}>{errors.amount}</p>}
                                             </Form.Group>
                                             <Form.Group controlId="Rate" className='mb-3'>
                                                 <Form.Label>อัตราดอกเบี้ย</Form.Label>
-                                                <Form.Control type="number" onChange={e => setValues({ ...values, rate: e.target.value })} required/>
+                                                <Form.Control type="number" name="rate" onChange={handleChange} />
+                                                {errors.rate && <p style={{color: 'red'}}>{errors.rate}</p>}
                                             </Form.Group>
                                             <Form.Group controlId="startDate" className='mb-3'>
                                                 <Form.Label>วันที่กู้</Form.Label>
-                                                <Form.Control type="date" onChange={e => setValues({ ...values, start_date: e.target.value })} required/>
+                                                <Form.Control type="date" name="start_date" onChange={handleChange} />
+                                                {errors.start_date && <p style={{color: 'red'}}>{errors.start_date}</p>}
                                             </Form.Group>
                                         </Col>
                                     </Row>
