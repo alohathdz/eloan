ab31663bb44a7ed8db6b4a64342626b149116590
alohathdz <57053156+alohathdz@users.noreply.github.com>
2023-09-08 11:58:54 +0700

    etc

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -3,39 +3,8 @@ import axios from 'axios';
 
 function Home() {
 
-    const [data, setData] = useState([])
-
-    useEffect(() => {
-        axios.get('http://localhost:8081/')
-            .then(res => console.log(res))
-            .catch(err => console.log(err));
-    }, [])
-
     return (
         <div>
-            <div>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>ลำดับ</th>
-                            <th>ชื่อ</th>
-                            <th>จัดการ</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {data.map((member, index) => {
-                            return <tr key={index}>
-                                <td>{member.member_id}</td>
-                                <td>{member.name}</td>
-                                <td>
-                                    <button>แก้ไข</button>
-                                    <button>ลบ</button>
-                                </td>
-                            </tr>
-                        })}
-                    </tbody>
-                </table>
-            </div>
         </div>
     )
 }
